1eb26f2f66893e0cffe226b738ca7ee37a39d16a
Stephen160623 <136847798+Stephen160623@users.noreply.github.com>
2023-06-30 14:07:11 -0300

    Add files via upload

---
+++ questao1.12.py
@@ -0,0 +1,14 @@
+# Atribuição de variáveis
+
+# Entrada de dados
+
+r = float(input("Raio do círculo: "))
+
+#Processamento de dados
+
+c = 2*3.141592653589793*r
+a = 3.141592653589793*(r**2)
+
+# Saída de dados
+
+print("A circunferência do círculo com o raio apontado pelo usuário é: " + str(c) + " e a sua área é: " + str(a))
